52cc93d0329f1255c7750079950d642871f202e0
Debra <dsparr1010@gmail.com>
2020-07-15 16:38:16 -0500

    added POST test view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class TestView(APIView):
     """Test API View"""
+    serializer_class = serializers.TestSerializer
 
     def get(self, request, format=None):
         """Returns a list of API features"""
@@ -15,4 +19,18 @@ class TestView(APIView):
         ]
 
         return Response({'message': 'Heeyyyy', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        print(serializer)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message' : message})
+
+        return Response(serializer.errors, 
+                        status=status.HTTP_400_BAD_REQUEST
+                        )
 
