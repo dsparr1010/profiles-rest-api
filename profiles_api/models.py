bc395e77a51c56aa9145fe29fdd6da30f328d329
Debra <dsparr1010@gmail.com>
2020-07-16 17:57:30 -0500

    added user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from .managers import UserProfileManager
+from django.conf import settings
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Custom model for users in the db"""
@@ -26,3 +27,13 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return string representation of our user"""
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return model as string"""
+        return self.status_text
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,6 +5,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('testing-viewset', views.TestViewSet, base_name='testing-viewset') # only specify queryset if you don't have a queryset on ViewSet or you want to override the name associated with the queryset
 router.register('profile', views.UserProfileViewSet) # don't need base_name because a queryset was provided
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('testing-apiview/', views.TestView.as_view()),
